8ac2e67c287afd571bd83013b922564c0395117b
Dániel Morvai <daniel.morvai@atolldev.hu>
2020-07-19 19:59:27 +0200

    Init app

---
+++ paintball-demo-app/app/src/main/java/demo/app/paintball/MainActivity.kt
@@ -0,0 +1,11 @@
+package demo.app.paintball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
